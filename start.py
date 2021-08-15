5de8e66a8b4182dbd3b84e3d06a32c343056da52
Gaby Scheleider <gayscheleider@gmail.com>
2021-08-15 19:18:07 -0300

    Testes

--- start.py
+++ start.py
@@ -1,18 +1,24 @@
 """ Strings """
-# Verificar se um texto está contido dentro do outro:
-# Verificação IN é case sensitive
-print('y' in 'Gabriely') #Lê-se 'y' está contido em 'Gabriely'? -> true
+## Verificar se um texto está contido dentro do outro:
+## Verificação IN é case sensitive
 
-print('Y' in 'Gabriely') #Mas false se 'Y'
+# print('y' in 'Gabriely') #Lê-se 'y' está contido em 'Gabriely'? -> true
+
+# print('Y' in 'Gabriely') #Mas false se 'Y'
 
 """ Variáveis """
-qtde_vendas = 2323
+# qtde_vendas = 2323
 
-print(qtde_vendas)
+# print(qtde_vendas)
 
 """ Pegando informações do usuário """
-nome = input('Qual o seu nome?')
+# nome = input('Qual o seu nome?')
 
-sobrenome = input('Qual o seu sobrenome?')
+# sobrenome = input('Qual o seu sobrenome?')
 
-print(nome + ' ' + sobrenome)
+# print(nome + ' ' + sobrenome)
+
+""" Variáveis e tipos de variáveis """
+faturamento = 1000
+print (type(faturamento))
+
